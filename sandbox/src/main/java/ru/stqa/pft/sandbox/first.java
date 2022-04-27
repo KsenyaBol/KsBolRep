cacbe00dc2cc8357442cf732267b6178b4504318
KsenyaBol <104258704+KsenyaBol@users.noreply.github.com>
2022-04-27 09:25:07 +0300

    добавлены новые классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/first.java
@@ -5,24 +5,19 @@ public class First {
     hello("user");
     hello("Ksusha");
 
-    double len = 5;
-    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+    Point p1 = new Point(3, 4);
+    Point p2 = new Point(6, 9);
+    System.out.println("Расстояние между двумя точками с координатами " + p1.x1 + "," + p1.y1 + " и " + p2.x2 + "," + p2.y2 + " = " + d.distance());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
-
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(double l) {
-    return l * l;
-  }
-  public static double area(double a, double b) {
-    return a * b;
   }
 }
 
