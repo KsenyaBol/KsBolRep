8bb54b78ad66bcf7fbcad2778942235951cc716c
KsenyaBol <104258704+KsenyaBol@users.noreply.github.com>
2022-04-30 23:01:32 +0300

    задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Pointslaunch.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Pointslaunch.java
@@ -2,7 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class Pointslaunch {
   public static void main(String[] args){
-    Point p = new Point(2,6, 5, 10);
-    System.out.println("расстояние между двумя точками с координаами (" +p.x1 + ";" +p .y1 + ") и (" + p.x2 + ";" + p.y2 + ") = " + p.distance());
+    Point p1 = new Point(2, 6);
+    Point p2 = new Point(5, 10);
+    System.out.println("расстояние между двумя точками с координаами (" +p1.x + ";" +p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
   }
 }
